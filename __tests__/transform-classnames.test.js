a20f3df2fa67eb865184c1d2d4af89bfa7fecec1
Arnab Chatterjee <139915335+ArnabChatterjee2001@users.noreply.github.com>
2023-11-15 02:15:31 +0530

    first commit

---
+++ __tests__/transform-classnames.test.js
@@ -0,0 +1,34 @@
+const babel = require('@babel/core');
+const plugin = require('./../src/transform-classname');
+
+const transformCode = (code) => {
+  return babel.transform(code, {
+    plugins: [plugin],
+  }).code;
+};
+
+describe('Babel Plugin Transform Classnames', () => {
+  it('transforms simple static class name', () => {
+    const input = '<div className="example" />';
+    const output = transformCode(input);
+    expect(output).toMatchSnapshot();
+  });
+
+  it('transforms dynamic class name with hyphen', () => {
+    const input = '<div className={dynamicClassName} />';
+    const output = transformCode(input);
+    expect(output).toMatchSnapshot();
+  });
+
+  it('does not transform dynamic class name without hyphen', () => {
+    const input = '<div className={dynamicClassName} />';
+    const output = transformCode(input);
+    expect(output).toMatchSnapshot();
+  });
+
+  it('leaves other JSX attributes unchanged', () => {
+    const input = '<div id="myDiv" data-custom="value" />';
+    const output = transformCode(input);
+    expect(output).toMatchSnapshot();
+  });
+});
